fdfc038daf047c5c2da91d5aba40a916d585e6dc
Jim Nelin <jim.nelin@makerspace.se>
2013-08-28 23:04:24 +0200

    Updated dblayout

--- database.sql
+++ database.sql
@@ -1,11 +1,19 @@
 
-CREATE TABLE `urls` (
+CREATE TABLE IF NOT EXISTS `urls` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
+  `url` varchar(2048) NOT NULL,
   `created` int(11) NOT NULL,
-  `ip` varchar(255) NOT NULL,
+  `ip` varchar(39) NOT NULL,
+  `visits` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `url` (`url`),
   KEY `ip` (`ip`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `url_id` bigint(20) NOT NULL,
+  `ip` varchar(39) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `url_id` (`url_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
